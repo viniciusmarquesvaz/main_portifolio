e16ce402dae50d3a7cdf064cc071ee4c833a12ba
vinicius <viniciusmanleft@gmail.com>
2023-09-15 14:56:48 -0300

    update portifolio

--- js/generateskills.js
+++ js/generateskills.js
@@ -13,7 +13,34 @@
     { iconClass: "far fa-file-excel", title: "Excel" },
     { iconClass: "mdi mdi-language-typescript", title: "TypeScript" }
   ];
-  
+   [
+   {nome: "Otavio Figueiredo",
+   nivel_de_confianca:"medio para alto",
+   profissao:"eletricista extremamente profissional",
+   serventia:"auxiliar no sistema da estufa do bunker, ajudar a escolher melhores equipamentos elétricos Led e etc, e auxiliar na economia elétrica",
+   laço_maior_de_contato:"Mateus",
+   },
+   {
+    nome:"Alexandra Dias",
+    nivel_de_confianca:"medio baixo",
+    profissao: "Arquiteta Urbanista",
+    serventia: "ajudar no projeto da fazenda afastada e no bunker, pode projetar e ajudar com consultoria dos materiais",
+    laço_maior_de_contato:"Vinícius"
+   },
+   {
+    nome:"Pedro Almeida",
+    nivel_de_confianca: "Alta",
+    profissão: "Desenvolvedor BackEnd",
+    serventia: "Pai é Advogado Criminalista, podemos consultar dúvidas e talvez podemos ser clinetes caso haja alguma merda",
+    laço_maior_de_contato:"Vinícius"
+   },
+   {
+    nome: "Cara q vende cnh digital e conta mp +55 34 9981-8581",
+    nivel_de_confianca: "nao tem como mensurar",
+    profissao: "nao sei",
+    serventia: "fornecer cnh digital e a conta mp e outros recursos talvez",
+   }
+  ]
   const container = document.getElementById("containerSkills");
   
   skills.forEach(skill => {
